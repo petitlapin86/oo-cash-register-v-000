5ddfa48a42fcf92fcfecb6ba8bd48b538045f309
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 15:00:06 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,15 +5,15 @@ attr_accessor :items, :discount, :total, :last_transaction
 def initialize(discount=0) # optionally takes a employee discount on initialization
   @total = 0 #setting an instance variable total to zero
   @discount = discount
-  @items = [] #initialize an empty array to hold items 
+  @items = [] #initialize an empty array to hold items
 end
 
 def add_item(title, price, quantity=1) #accepts a title, price and optional quantity
-     self.total += amount * quantity #increases total
+     self.total += price * quantity #increases total
      quantity.times do
        items << title #pushes item title to array of items
      end
-     self.last_transaction = amount * quantity #doesnt forget previous total
+     self.last_transaction = price * quantity #doesnt forget previous total
    end
 
 
