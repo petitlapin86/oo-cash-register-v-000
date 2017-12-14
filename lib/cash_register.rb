dde5180177eeb1e60dbeb9f30cd0fdcd331140ef
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 14:56:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,16 @@
 
 class CashRegister #creating my new class
-attr_accessor :items, :discount, :total, :title, :price 
+attr_accessor :items, :discount, :total, :title, :price
 
 def initialize(discount=0) # optionally takes a employee discount on initialization
   @total = 0 #setting an instance variable total to zero
   @discount = discount
 end
 
-def add_item(title, price, quantity=1) #accepts a title, price and optional quantity 
-     self.total += amount * quantity #increases total 
+def add_item(title, price, quantity=1) #accepts a title, price and optional quantity
+     self.total += amount * quantity #increases total
      quantity.times do
-       items << title #pushes item title to array of items 
+       items << title #pushes item title to array of items
      end
      self.last_transaction = amount * quantity #doesnt forget previous total 
    end
