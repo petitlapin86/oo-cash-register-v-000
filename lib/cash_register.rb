836d8dbca5b5bae343d776f835ac35deaa1ab188
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 14:51:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,10 @@
 
 class CashRegister #creating my new class
-attr_accessor :discount, :total
+attr_accessor :discount, :total, :title, :price 
 
-def initialize(discount=0)
-  @total = 0
-  @discount = discount
+def initialize(discount=0) # optionally takes a employee discount on initialization
+  @total = 0 #setting an instance variable total to zero 
+  @discount = discount 
 end
 
 end
