6bb8115ad685e2b6fb5d72780f80fafe1f5e3d75
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 14:59:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,6 +5,7 @@ attr_accessor :items, :discount, :total, :last_transaction
 def initialize(discount=0) # optionally takes a employee discount on initialization
   @total = 0 #setting an instance variable total to zero
   @discount = discount
+  @items = []
 end
 
 def add_item(title, price, quantity=1) #accepts a title, price and optional quantity
